d6ebcf3379fc85085ed72b9f431567eda00f77bb
spatwardhan <shree.pat18@gmail.com>
2023-07-09 14:27:45 +0800

    Input and validate grid size

---
+++ GameOfLife.Core/Grid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GameOfLife.Core
+{
+    public class Grid
+    {
+        public int Width {get;set;}
+        public int Height {get;set;}
+
+        public Grid(int width, int height)
+        {
+            if(width <= 0 || width > 25 || height <= 0 || height > 25)
+            throw new ArgumentException("Dimensions must be between 1 to 25 inclusive!");
+
+            this.Width = width;
+            this.Height = height;
+        }
+    }
+}
--- GameOfLife.UI/Program.cs
+++ GameOfLife.UI/Program.cs
@@ -1,33 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
+using GameOfLife.Core;
 
 namespace GameOfLife.UI
 {
     public class Program
     {
         static readonly string MainPrompt = $"Welcome to Conway's Game of Life{Environment.NewLine}[1] Specify grid size{Environment.NewLine}[2] Specify number of generation{Environment.NewLine}[3] Specify initial live cells{Environment.NewLine}[4] Run{Environment.NewLine}Please enter your selection";
+        static readonly string DimensionsFormat = @"(\d{1,2})\s(\d{1,2})";
+        static Grid grid = default;
 
         static void Main(string[] args)
         {            
             Console.WriteLine(MainPrompt);
-            var choice = Console.Read();
+            var choice = Console.ReadLine();            
 
             switch(choice)
             {
-                case '1': SetGrid();
+                case "1": SetGrid();
                 break;
-                case '2': SetGenerations();
+                case "2": SetGenerations();
                 break;
-                case '3': InitializeGrid();
+                case "3": InitializeGrid();
                 break;
                 default: break;
             }
         }
 
         private static void SetGrid()
-        {
-
+        {                
+            var regex = new Regex(DimensionsFormat);
+            Console.WriteLine("Please enter grid size in w h format (example: 10 15):");
+            var dimensions = Console.ReadLine();
+            try
+            {                
+                var match = regex.Match(dimensions);
+                var width = int.Parse(match.Groups[1].Value);
+                var height = int.Parse(match.Groups[2].Value);
+                grid = new Grid(width,height);
+            }
+            catch(ArgumentException aex)
+            {
+                Console.WriteLine(aex.Message);
+                SetGrid();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                SetGrid();
+            }
         }
 
         private static void SetGenerations()
