3807da2eb67c79af415de7caf12d743f29b31be3
spatwardhan <shree.pat18@gmail.com>
2023-07-09 13:22:24 +0800

    UI Skeleton

--- GameOfLife.UI/Program.cs
+++ GameOfLife.UI/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GameOfLife.UI
 {
-    class Program
+    public class Program
     {
+        static readonly string MainPrompt = $"Welcome to Conway's Game of Life{Environment.NewLine}[1] Specify grid size{Environment.NewLine}[2] Specify number of generation{Environment.NewLine}[3] Specify initial live cells{Environment.NewLine}[4] Run{Environment.NewLine}Please enter your selection";
+
         static void Main(string[] args)
+        {            
+            Console.WriteLine(MainPrompt);
+            var choice = Console.Read();
+
+            switch(choice)
+            {
+                case '1': SetGrid();
+                break;
+                case '2': SetGenerations();
+                break;
+                case '3': InitializeGrid();
+                break;
+                default: break;
+            }
+        }
+
+        private static void SetGrid()
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        private static void SetGenerations()
+        {
+
+        }
+
+        private static void InitializeGrid()
+        {
+
         }
     }
 }
