0c944585736d34b073e1e591f012579be4964eb8
spatwardhan <shree.pat18@gmail.com>
2023-07-22 19:49:07 +0800

    Added more tests

--- GameOfLife.Tests/SetGenerationsCommandTests.cs
+++ GameOfLife.Tests/SetGenerationsCommandTests.cs
@@ -38,5 +38,23 @@ namespace GameOfLife.Tests
             Assert.Equal("Please enter valid input!", result.MessageText);
             Assert.Equal(0, _settings.Generations);
         }
+
+        [Theory]
+        [InlineData(Status.INVALID, "not a number", "Please enter valid input!")]
+        [InlineData(Status.OUT_OF_RANGE, "50", "Number of generations must be between 3 and 20!")]
+        public void ShouldRetainLastSetValuesOnSubsequentBadInput(Status status, string commandText, string messageText)
+        {
+            var result1 = _command.Execute(_settings, "10");
+
+            Assert.Equal(Status.VALID, result1.Status);
+            Assert.Null(result1.MessageText);
+            Assert.Equal(10, _settings.Generations);
+
+            var result2 = _command.Execute(_settings, commandText);
+
+            Assert.Equal(status, result2.Status);
+            Assert.Equal(messageText, result2.MessageText);
+            Assert.Equal(10, _settings.Generations);
+        }
     }
 }
--- GameOfLife.Tests/SetGridCommandTests.cs
+++ GameOfLife.Tests/SetGridCommandTests.cs
@@ -30,5 +30,36 @@ namespace GameOfLife.Tests
             Assert.Equal(25, _settings.Grid.Width);
             Assert.Equal(25, _settings.Grid.Height);
         }
+
+        [Fact]
+        public void ShouldRejectInvalidInput()
+        {
+            var result = _command.Execute(_settings, "bad input");
+
+            Assert.Equal(Status.INVALID, result.Status);
+            Assert.Equal("Input string was not in a correct format.", result.MessageText);
+            Assert.Equal(25, _settings.Grid.Width);
+            Assert.Equal(25, _settings.Grid.Height);
+        }
+
+        [Theory]
+        [InlineData(Status.INVALID,"bad input", "Input string was not in a correct format.")]
+        [InlineData(Status.OUT_OF_RANGE, "50 50", "Dimensions must be between 1 to 25 inclusive!")]
+        public void ShouldRetainLastSetValuesOnSubsequentBadInput(Status status, string commandText, string messageText)
+        {
+            var result1 = _command.Execute(_settings, "10 10");
+
+            Assert.Equal(Status.VALID, result1.Status);
+            Assert.Null(result1.MessageText);
+            Assert.Equal(10, _settings.Grid.Width);
+            Assert.Equal(10, _settings.Grid.Height);
+
+            var result2 = _command.Execute(_settings, commandText);
+
+            Assert.Equal(status, result2.Status);
+            Assert.Equal(messageText, result2.MessageText);
+            Assert.Equal(10, _settings.Grid.Width);
+            Assert.Equal(10, _settings.Grid.Height);
+        }
     }
 }
